00ea4869826c15f672efd672fae6505a55d87fb1
jiho <kjs844@gmail.com>
2026-04-16 15:36:52 +0900

    20260416supabase새로

---
+++ src/test/java/com/joe/kosmo_post/KosmoPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joe.kosmo_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosmoPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

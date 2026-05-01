c9b983d4b3be35603cac26f70aa08e985181d00a
jiho <kjs844@gmail.com>
2026-05-01 16:05:45 +0900

    20260430
    00

--- src/main/java/com/joe/kosmo_post/app/member/MemberController.java
+++ src/main/java/com/joe/kosmo_post/app/member/MemberController.java
@@ -1,21 +1,14 @@
 package com.joe.kosmo_post.app.member;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member/*")
-
-
 public class MemberController {
 
-	@Autowired
-	private MemberService memberService;
-	
-	@GetMapping("join")
-	
-	
+    @GetMapping("join")
+    public void join() throws Exception {
+    }
 }
